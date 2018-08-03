6bf165024783deaa62eda9b72db476e8b539b26d
Tuomas Salo <tuomas.salo@iki.fi>
2018-08-04 01:52:50 +0300

    Fix watchdog script dependency, allow fail at first run

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 # fail on error
 set -e
 
+apt-get update
+apt-get -y upgrade
+apt-get install libjson-perl
+
 cd /tmp/
 
 git clone https://github.com/tuomassalo/victron-mk2-can-bridge.git
@@ -12,7 +16,8 @@ mv host-scripts /opt/
 
 # The first container launch is slow, because the images are built.
 # Do it before starting the watchdog.
-/opt/host-scripts/launch-container.sh
+# NB: First launch is allowed to fail. We still want to finish this script.
+/opt/host-scripts/launch-container.sh || /bin/true
 
 mv cron-entry.txt /etc/cron.d/bridge-watchdog
 
